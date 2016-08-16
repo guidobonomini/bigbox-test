708a328c9f8ad1954feeaf45251c97648087114c
Guido <bono_9475@hotmail.com>
2016-08-16 16:25:31 -0300

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -124,4 +124,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
