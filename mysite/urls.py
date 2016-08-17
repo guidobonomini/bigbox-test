4448a0e019d096a7f80b8c1caa40fa3be4babebc
Guido <bono_9475@hotmail.com>
2016-08-17 02:29:23 -0300

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import include, url
+from django.views.generic.base import RedirectView
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^$', lambda r: HttpResponseRedirect('polls/')),
+    url(r'^$', RedirectView.as_view(url='/home/')),
     url(r'^polls/', include('polls.urls')),
     url(r'^admin/', admin.site.urls),
 ]
